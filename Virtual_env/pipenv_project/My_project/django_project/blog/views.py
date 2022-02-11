4ec4b8e7dfdc2002d545b6065e6d8bf7506ac4e9
Ali Zain <thealizain@outlook.com>
2022-02-11 09:57:44 -0500

    python added

--- Virtual_env/pipenv_project/My_project/django_project/blog/views.py
+++ Virtual_env/pipenv_project/My_project/django_project/blog/views.py
@@ -4,10 +4,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+#below is a jason data
+
+posts = [
+	{
+		'author': 'John',
+		'title': 'post 1',
+		'content':'content for blog post 1',
+		'date_posted':'Feb 9, 2022'
+	},
+	{
+		'author': 'Tom',
+		'title': 'post 2',
+		'content':'content for blog post 2',
+		'date_posted':'Feb 10, 2022'
+	}
+
+	
+]
+
 def home(request):
-	return render(request,'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request,'blog/home.html', context)
 
 def about(request):
-	return render(request,'blog/about.html')
+	return render(request,'blog/about.html', {'title':'About'})
 
  
